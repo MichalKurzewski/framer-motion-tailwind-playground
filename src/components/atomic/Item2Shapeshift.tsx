6c18579b48c4b9e6da01c67eb2b21c0afa520fc6
Michal Kurzewski <kurzewski.m@gmail.com>
2023-02-18 00:13:00 +0000

    minor change to 2nd item

--- src/components/atomic/Item2Shapeshift.tsx
+++ src/components/atomic/Item2Shapeshift.tsx
@@ -8,11 +8,11 @@ const Item2ShapeShift = () => {
       <motion.div
         className="div-item"
         animate={{
-          scale: [1, 1.5, 1.5, 1, 1, 1, 1, 1],
-          scaleX: [1, 1, 1, 1, 1, 1.5, 0.7, 1],
-          scaleY: [1, 1, 1, 1, 1, 0.7, 1.5, 1],
+          scale: [0.75, 1.3, 1.3, 1, 1, 1, 1, 1],
+          scaleX: [1, 1, 1, 1, 1, 1.25, 0.75, 1],
+          scaleY: [1, 1, 1, 1, 1, 0.75, 1.25, 1],
           rotate: [0, 0, 270, 270, 0, 0, 0, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%", "0%", "0%", "20%"],
+          borderRadius: ["20%", "10%", "50%", "50%", "20%", "0%", "0%", "20%"],
           transition: { duration: 8, repeat: Infinity, repeatType: "mirror" },
         }}
       />
