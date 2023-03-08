a8542766eb3c1f301ce77d693fb6e0d33b4c0fd9
Michal Kurzewski <kurzewski.m@gmail.com>
2023-03-08 23:07:17 +0000

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/framer-motion-tailwind-playground/',
+  base: "/",
   plugins: [svgr(), react()],
   server: {
     port: 3000,
