5580dbe5973c581bcde8c5fd22aaaf37891f96fe
Michal Kurzewski <kurzewski.m@gmail.com>
2023-03-18 00:57:37 +0000

    Item test

---
+++ src/components/atomic/Molecules/Item.test.tsx
@@ -0,0 +1,35 @@
+import { test } from "vitest";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Item from "./Item";
+
+test("renders without crashing", () => {
+  render(<Item label="Test Label" />);
+  expect(screen.getByText("Test Label")).toBeInTheDocument();
+});
+
+test("renders children", () => {
+  render(
+    <Item label="Test Label">
+      <div data-testid="test-child" />
+    </Item>
+  );
+  expect(screen.getByTestId("test-child")).toBeInTheDocument();
+});
+
+test("triggers onClick handler", async () => {
+  let clicked = false;
+  const handleClick = () => {
+    clicked = true;
+  };
+  render(<Item label="Test Label" onClick={handleClick} />);
+
+  fireEvent.click(screen.getByText("Test Label"));
+  expect(clicked).toBe(true);
+});
+
+test("applies additional styling", () => {
+  render(<Item label="Test Label" additionalStyling="bg-red-500" />);
+  const item = screen.getByText("Test Label");
+  expect(item.parentElement).toHaveClass("bg-red-500");
+});
