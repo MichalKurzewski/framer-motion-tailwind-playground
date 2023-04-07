8fa3763c000646ca9764ae0a2b5e1ceabf93d4bb
Michal Kurzewski <kurzewski.m@gmail.com>
2023-04-08 00:31:52 +0100

    tests descriptions

--- e2eTests/flow.spec.ts
+++ e2eTests/flow.spec.ts
@@ -1,10 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("test", () => {
-  test("test", async ({ page }) => {
+test.describe("Framer Motion Tailwind Playground", () => {
+  test("should navigate to different pages on button click", async ({
+    page,
+  }) => {
+    // Navigate to the home page of the Framer Motion Tailwind Playground
     await page.goto("http://localhost:4000/framer-motion-tailwind-playground/");
+
+    // Click the "First" button and verify that the URL changes
     await page.getByRole("button", { name: "First" }).click();
     await expect(page).toHaveURL(/.*First/);
+
+    // Click the "Second" button and verify that the URL changes again
     await page.getByRole("button", { name: "Second" }).click();
     await expect(page).toHaveURL(/.*Second/);
   });
