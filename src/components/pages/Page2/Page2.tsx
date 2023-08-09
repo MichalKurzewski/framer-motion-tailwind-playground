18fee79a47136a9b6b8c336206e3564607d4036d
Michal Kurzewski <kurzewski.m@gmail.com>
2023-08-09 15:18:34 +0100

    tidy up page 2

--- src/components/pages/Page2/Page2.tsx
+++ src/components/pages/Page2/Page2.tsx
@@ -10,13 +10,13 @@ const Page2: React.FC = (): JSX.Element => {
     () => import("../../atomic/Molecules/Page2Items/Item11Lottie")
   );
   return (
-    <>
-      <motion.div
-        className="container mx-auto flex p-10 justify-evenly flex-wrap gap-20 overflow-visible"
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
-        exit={{ opacity: 0, x: -50, transition: { duration: 0.5 } }}
-      >
+    <motion.div
+      className="container mx-auto flex p-10 justify-evenly flex-wrap gap-20 overflow-visible"
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
+      exit={{ opacity: 0, x: -50, transition: { duration: 0.5 } }}
+    >
+      <div className="container mx-auto flex p-10 justify-evenly flex-wrap gap-20 overflow-visible">
         <LayoutGroup id="page2">
           <Item6LoadingSpinner />
           <Suspense fallback={<div>Loading...</div>}>
@@ -24,18 +24,14 @@ const Page2: React.FC = (): JSX.Element => {
           </Suspense>
           <Item12OnHoverCircles />
         </LayoutGroup>
-      </motion.div>
-      <motion.div
-        className="container mx-auto flex p-10 justify-evenly flex-wrap gap-20"
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
-        exit={{ opacity: 0, x: -50, transition: { duration: 0.5 } }}
-      >
-        <Item14PhoneApp />
-      </motion.div>
-    </>
+      </div>
+      <div className="container mx-auto flex p-10 justify-evenly flex-wrap gap-20">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Item14PhoneApp />
+        </Suspense>
+      </div>
+    </motion.div>
   );
 };
 
 export default Page2;
-
